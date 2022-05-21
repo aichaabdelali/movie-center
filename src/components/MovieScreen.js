b54e4c8e9312912df0ff19d37b8503c0b445407e
aichaabdelali <aicha.abdelali6@gmail.com>
2022-05-22 01:23:21 +0200

    movie screen and adding similar movies

--- src/components/MovieScreen.js
+++ src/components/MovieScreen.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
-import { Image, Container, Row, Col, Card, Badge } from "react-bootstrap";
-import { useParams } from "react-router-dom";
+import { Row, Col, Card, Container, Image, ListGroup } from "react-bootstrap";
+import { Link, useParams } from "react-router-dom";
 import { FaStar } from "react-icons/fa";
 
 const MovieScreen = () => {
@@ -8,11 +8,18 @@ const MovieScreen = () => {
   const IMG_URL = "https://image.tmdb.org/t/p/w1280";
 
   const [movie, setMovie] = useState({});
+  const [similars, setSimilars] = useState({});
 
   const fetchMovie = async (url) => {
     const res = await fetch(url);
     const data = await res.json();
     return data;
+  };
+
+  const fetchSimilar = async (url) => {
+    const res = await fetch(url);
+    const data = await res.json();
+    return data.results.slice(0, 4);
   };
 
   useEffect(() => {
@@ -26,51 +33,69 @@ const MovieScreen = () => {
     getMovie();
   }, [params.id]);
 
-  console.log(movie);
+  useEffect(() => {
+    const getSimilar = async () => {
+      const similar_movies = await fetchSimilar(
+        `https://api.themoviedb.org/3/movie/${params.id}/similar?api_key=cb56581cd73993b93e4cd062650225b9&language=en-US`
+      );
+
+      setSimilars(similar_movies);
+    };
+    getSimilar();
+  }, [params.id]);
+
+  console.log(movie, similars);
 
   return (
-    <Container>
-      <Card variant="dark">
-        <Card.Body>
-          <Row>
-            <Col md={4}>
-              <Image
-                src={IMG_URL + movie.poster_path}
-                alt={movie.title}
-                fluid
-              />
-            </Col>
-            <Col md={7}>
-              <Card.Title className="title">{movie.title}</Card.Title>
-              <Card.Subtitle>
-                Original title : {movie.original_title}
-              </Card.Subtitle>
-              <Card.Text className="release-runtime">
-                {movie.release_date} . {movie.runtime}min
-              </Card.Text>
-              <Card.Text className="vote">
-                <FaStar
-                  style={{
-                    color: "#FFCE45",
-                    width: "28px",
-                    height: "28px",
-                    margin: "6px 10px 6px 0px",
-                  }}
-                />{" "}
-                <div>
+    <>
+      <Container>
+        <Link className="btn btn-dark my-4" to="/">
+          Go Back
+        </Link>
+        <Card variant="dark" style={{ width: "60vw", border: "none" }}>
+          <Card.Body>
+            <Row>
+              <Col md={3}>
+                <Card.Img
+                  src={IMG_URL + movie.poster_path}
+                  alt={movie.title}
+                  fluid
+                />
+              </Col>
+              <Col md={8}>
+                <Card.Title className="title">{movie.title}</Card.Title>
+                <Card.Subtitle>
+                  Original title : {movie.original_title}
+                </Card.Subtitle>
+                <Card.Text className="release-runtime">
+                  {movie.release_date} . {movie.runtime}min
+                </Card.Text>
+                <Card.Text className="vote">
+                  {" "}
                   {movie.vote_average} /10
                   <span className="vote-count">{movie.vote_count} votes</span>
-                </div>
-              </Card.Text>
-              <Card.Link href={movie.homepage} target="_blank">
-                {movie.title} : {movie.tagline}
-              </Card.Link>
-              <Card.Text className="overview">{movie.overview}</Card.Text>
-            </Col>
-          </Row>
-        </Card.Body>
-      </Card>
-    </Container>
+                </Card.Text>
+                <Card.Link href={movie.homepage} target="_blank">
+                  {movie.title} : {movie.tagline}
+                </Card.Link>
+                <Card.Text className="overview">{movie.overview}</Card.Text>
+              </Col>
+              <Col md={2}>
+                {similars.map((similar) => (
+                  <Image
+                    className="similar-poster"
+                    key={similar.id}
+                    src={IMG_URL + similar.poster_path}
+                    alt={IMG_URL + similar.title}
+                    fluid
+                  />
+                ))}
+              </Col>
+            </Row>
+          </Card.Body>
+        </Card>
+      </Container>
+    </>
   );
 };
 
